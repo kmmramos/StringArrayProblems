28d2640092466f2d28ffe135af4bc6871eeb5ee5
kmmramos <kmmramos@gmail.com>
2022-10-04 11:25:42 +0800

    10/4/2022 commit: add problems 5 and six

---
+++ StringArrayProblems/Logic/ProblemFive.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringArrayProblems.Logic
+{
+    public class ProblemFive
+    {
+        public static int CoinStackExchange(int[] A)
+        {
+            int arrayLength = A.Length;
+            int quotient = 0, dividend, remainder, lastRemainder = 0, partialTotal = 0;
+            //return the 1 or 0, if that's the only value
+            if (arrayLength <= 1 && A[0] <= 1) return A[0];
+            //return 0 if null
+            else if (A == null) return 0;
+            //loop inside array or if the only element is greater than 1
+            else
+            {
+                //set the only element to be processed if greater than 1
+                if (arrayLength <= 1 && A[0] > 1) quotient = A[0];
+                //loop inside an array or continue execution as long as the value/quotient is greater than 1
+                int index = 0;
+                while (index < arrayLength || quotient >= 2)
+                {
+                    if (arrayLength >= 2 && index < arrayLength)
+                    {
+                        quotient = quotient + A[index];
+                    }
+                    //coin stack exchange operation, the main logic
+                    dividend = quotient;
+                    quotient = dividend / 2;
+                    remainder = dividend % 2;
+                    partialTotal += remainder;
+                    index++;
+                }
+                //if there's a last remaining value or quotient that's less than 2, add it as the lastRemainder
+                if (quotient <= 1) lastRemainder = quotient;
+
+                //compute for the total got from lastremainder (if there's value) plus the computer partialTotal
+                int total = lastRemainder + partialTotal;
+                return total;
+            }
+        }
+    }
+}
---
+++ StringArrayProblems/Logic/ProblemSix.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringArrayProblems.Logic
+{
+    public class ProblemSix
+    {
+        public static int NextGreater(int N)
+        {
+            string nextNumberString = (N + 1).ToString();
+            Stack<int> temporaryStack = new Stack<int>();
+            //loop through number string
+            int i = 0;
+            for (; i < nextNumberString.Length; i++)
+            {
+                var currentDigit = nextNumberString[i];
+                if (temporaryStack.Count == 0 || temporaryStack.Peek() != (currentDigit - '0')) //if stack is empty or if previous != current digit
+                {
+                    temporaryStack.Push(currentDigit - '0'); //continue pushing the current digit to the temporaryStack
+                }
+                else if (temporaryStack.Peek() == 9) //handle the edge case which is the number containing digit 9
+                {
+                    temporaryStack.Pop(); //remove current digit which is 9
+
+                    if (temporaryStack.Count == 0) //if stack is now empty
+                    {
+                        //recreate temporaryStack
+                        temporaryStack.Push(1);
+                        temporaryStack.Push(0);
+                        temporaryStack.Push(1);
+                    }
+                    else
+                    {
+                        //continue by pushing the previous digit to the stack plus 1
+                        var top = temporaryStack.Pop();
+                        temporaryStack.Push(top + 1);
+                        temporaryStack.Push(0);
+                        temporaryStack.Push(1);
+                    }
+                    break;
+                }
+                else
+                {
+                    //last value from temporaryStack equals the current digit
+                    temporaryStack.Push((currentDigit - '0') + 1);
+                    break;
+                }
+            }
+            //continue pushing to temporary stack if there are remaining digits which we haven't checked yet
+            i++;
+            int insert = 0;
+            while (i < nextNumberString.Length)
+            {
+                temporaryStack.Push(insert);
+                if (insert == 0)
+                {
+                    insert = 1;
+                }
+                else
+                {
+                    insert = 0;
+                }
+                i++;
+            }
+            //since stack IS LIFO, we need to reverse its order
+            int finalResult = 0;
+            var arr = temporaryStack.ToArray().Reverse();
+            foreach (var a in arr)
+            {
+                finalResult = finalResult * 10 + a;
+            }
+            //check if the final result has repeating consecutive digit
+            string finalResultString = finalResult.ToString();
+            for (int j = 0; j < finalResultString.Length - 1; j++)
+            {
+                //once this condition is satisfied, it means we need to perform finding NextGreater int again
+                if (finalResultString[j] == finalResultString[j + 1])
+                {
+                    //perform recursion, execute the method again to find the next greater number
+                    return NextGreater(finalResult);
+                }
+            }
+            //return final result
+            return finalResult;
+        }
+
+    }
+}
